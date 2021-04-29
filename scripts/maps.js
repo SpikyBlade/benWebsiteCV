aa01424dfc6247f4cfaee9f7b269554fdc314141
SpikyBlade <SpikyBlade@hotmail.com>
2021-04-29 17:18:30 +0200

    uploaded project

---
+++ scripts/maps.js
@@ -0,0 +1,258 @@
+mapboxgl.accessToken = "pk.eyJ1Ijoic3Bpa3libGFkZSIsImEiOiJja28xZzl0ZzEwOGVoMndvMzZwMXY3bmphIn0.hj_cu7YbbFa4HWCiEy-New";
+
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+});
+
+function successLocation(position) {
+    console.log(position);
+    setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+    setupMap([-2.24, 53.48]);
+}
+
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 15
+    });
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+
+    var directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+      });
+      
+      map.addControl(directions, 'top-left');
+
+}
+
+
+/*
+// Add in cordinates
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-79.4512, 43.6568],
+    zoom: 8
+  });
+  
+  // Given a query in the form "lng, lat" or "lat, lng"
+   //* returns the matching geographic coordinate(s)
+   //* as search results in carmen geojson format,
+   //* https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
+  var coordinatesGeocoder = function(query) {
+    // Match anything which looks like
+    // decimal degrees coordinate pair.
+    var matches = query.match(
+      /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+    );
+    if (!matches) {
+      return null;
+    }
+  
+    function coordinateFeature(lng, lat) {
+      return {
+        center: [lng, lat],
+        geometry: {
+          type: 'Point',
+          coordinates: [lng, lat]
+        },
+        place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+        place_type: ['coordinate'],
+        properties: {},
+        type: 'Feature'
+      };
+    }
+  
+    var coord1 = Number(matches[1]);
+    var coord2 = Number(matches[2]);
+    var geocodes = [];
+  
+    if (coord1 < -90 || coord1 > 90) {
+      // must be lng, lat
+      geocodes.push(coordinateFeature(coord1, coord2));
+    }
+  
+    if (coord2 < -90 || coord2 > 90) {
+      // must be lat, lng
+      geocodes.push(coordinateFeature(coord2, coord1));
+    }
+  
+    if (geocodes.length === 0) {
+      // else could be either lng, lat or lat, lng
+      geocodes.push(coordinateFeature(coord1, coord2));
+      geocodes.push(coordinateFeature(coord2, coord1));
+    }
+  
+    return geocodes;
+  };
+  
+  // Add the control to the map.
+  map.addControl(
+    new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      localGeocoder: coordinatesGeocoder,
+      zoom: 4,
+      placeholder: 'Try: -40, 170',
+      mapboxgl: mapboxgl
+    })
+  );
+
+*/
+
+
+/*
+// 3d Terrain
+var map = new mapboxgl.Map({
+    container: 'map',
+    zoom: 13.1,
+    center: [-114.34411, 32.6141],
+    pitch: 85,
+    bearing: 80,
+    style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y'
+});
+     
+map.on('load', function () {
+    map.addSource('mapbox-dem', {
+        'type': 'raster-dem',
+        'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        'tileSize': 512,
+        'maxzoom': 14
+    });
+
+// add the DEM source as a terrain layer with exaggerated height
+map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+     
+// add a sky layer that will show when the map is highly pitched
+    map.addLayer({
+        'id': 'sky',
+        'type': 'sky',
+        'paint': {
+            'sky-type': 'atmosphere',
+            'sky-atmosphere-sun': [0.0, 0.0],
+            'sky-atmosphere-sun-intensity': 15
+        }
+    });
+});
+*/
+
+/*
+
+// 3D model in maps
+var map = (window.map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    zoom: 18,
+    center: [148.9819, -35.3981],
+    pitch: 60,
+    antialias: true // create the gl context with MSAA antialiasing, so custom layers are antialiased
+}));
+     
+// parameters to ensure the model is georeferenced correctly on the map
+var modelOrigin = [148.9819, -35.39847];
+var modelAltitude = 0;
+var modelRotate = [Math.PI / 2, 0, 0];
+    
+var modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+    modelOrigin,
+    modelAltitude
+);
+     
+// transformation parameters to position, rotate and scale the 3D model onto the map
+var modelTransform = {
+    translateX: modelAsMercatorCoordinate.x,
+    translateY: modelAsMercatorCoordinate.y,
+    translateZ: modelAsMercatorCoordinate.z,
+    rotateX: modelRotate[0],
+    rotateY: modelRotate[1],
+    rotateZ: modelRotate[2],
+    // Since our 3D model is in real world meters, a scale transform needs to be
+    // applied since the CustomLayerInterface expects units in MercatorCoordinates.
+    
+    scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
+};
+     
+var THREE = window.THREE;
+    
+// configuration of the custom layer for a 3D model per the CustomLayerInterface
+var customLayer = {
+    id: '3d-model',
+    type: 'custom',
+    renderingMode: '3d',
+    onAdd: function (map, gl) {
+    this.camera = new THREE.Camera();
+    this.scene = new THREE.Scene();
+        
+    // create two three.js lights to illuminate the model
+    var directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(0, -70, 100).normalize();
+    this.scene.add(directionalLight);
+        
+    var directionalLight2 = new THREE.DirectionalLight(0xffffff);
+    directionalLight2.position.set(0, 70, 100).normalize();
+    this.scene.add(directionalLight2);
+        
+    // use the three.js GLTF loader to add the 3D model to the three.js scene
+    var loader = new THREE.GLTFLoader();
+    loader.load(
+    'https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf',
+    function (gltf) {
+        this.scene.add(gltf.scene);
+        }.bind(this)
+    );
+    this.map = map;
+        
+    // use the Mapbox GL JS map canvas for three.js
+    this.renderer = new THREE.WebGLRenderer({
+        canvas: map.getCanvas(),
+        context: gl,
+        antialias: true
+    });
+        
+    this.renderer.autoClear = false;
+    },
+    render: function (gl, matrix) {
+        var rotationX = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(1, 0, 0),
+            modelTransform.rotateX);
+        var rotationY = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(0, 1, 0),
+            modelTransform.rotateY);
+        var rotationZ = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(0, 0, 1),
+            modelTransform.rotateZ);
+        
+        var m = new THREE.Matrix4().fromArray(matrix);
+        var l = new THREE.Matrix4()
+        .makeTranslation(
+            modelTransform.translateX,
+            modelTransform.translateY,
+            modelTransform.translateZ)
+        .scale(
+        new THREE.Vector3(
+            modelTransform.scale,
+            -modelTransform.scale,
+            modelTransform.scale))
+        .multiply(rotationX)
+        .multiply(rotationY)
+        .multiply(rotationZ);
+            
+        this.camera.projectionMatrix = m.multiply(l);
+        this.renderer.resetState();
+        this.renderer.render(this.scene, this.camera);
+        this.map.triggerRepaint();
+    }
+};
+    
+map.on('style.load', function () {
+map.addLayer(customLayer, 'waterway-label');
+});
+
+*/
